6dc0a2b06d88c3fc4ace192d86e08004ab79498e
mz2 <matias.piipari@gmail.com>
2015-12-30 01:50:26 +0200

    Adds a shared scheme + framework target for Carthage.

--- extobjc/extobjc.h
+++ extobjc/extobjc.h
@@ -2,19 +2,29 @@
 //  extobjc.h
 //  extobjc
 //
-//  Created by Justin Spahr-Summers on 2010-11-09.
-//  Copyright (C) 2012 Justin Spahr-Summers.
-//  Released under the MIT license.
+//  Created by Matias Piipari on 30/12/2015.
 //
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for extobjc.
+FOUNDATION_EXPORT double extobjcVersionNumber;
+
+//! Project version string for extobjc.
+FOUNDATION_EXPORT const unsigned char extobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <extobjc/PublicHeader.h>
 
 #import "EXTADT.h"
 #import "EXTConcreteProtocol.h"
+#import "EXTCoroutine.h"
 #import "EXTKeyPathCoding.h"
 #import "EXTNil.h"
+#import "EXTRuntimeExtensions.h"
 #import "EXTSafeCategory.h"
 #import "EXTScope.h"
 #import "EXTSelectorChecking.h"
 #import "EXTSynthesize.h"
 #import "NSInvocation+EXT.h"
 #import "NSMethodSignature+EXT.h"
-
